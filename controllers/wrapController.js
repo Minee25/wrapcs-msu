52f66ae7962ca11d75c05cd0754cb0cd6c2afd5e
Minee <oakmuansuk123@gmail.com>
2024-05-20 21:26:13 +0700

    update

--- controllers/wrapController.js
+++ controllers/wrapController.js
@@ -37,3 +37,32 @@ for (let i = 0; i < wrapList.length; i++) {
     </div>
   </div>`
 };
+
+wrapBoxEl.innerHTML += `
+  <div class="card-item">
+    <div class="card-header"> 
+      <div class="card-background">
+        <img src="assets/img/empty.jpg" alt="">
+      </div>
+      <div class="card-logo">
+        <div class="card-logo-2">
+          <img src="assets/img/empty.jpg" alt="">
+        </div>
+      </div>
+    </div>
+
+    <div class="card-body"> 
+      <div class="card-title">
+        <h3 style="color: #ff2020;">ถ้าหากข้อมูลไม่ครบสามารถแจ้งได้ที่</h3>
+      </div>
+      <div class="card-description">
+        <a href="https://www.facebook.com/profile.php?id=100013052448629" target="_blank">
+          <i class="fa-brands fa-facebook"></i>&nbsp;Thaksin
+        </a>
+        <a href="https://www.instagram.com/oxkqz._25" target="_blank">
+          <i class="fa-brands fa-square-instagram"></i>&nbsp;oxkqz.25
+        </a>
+      </div> 
+    </div>
+  </div>
+`;
